f7071916e109e1b0daa077de0e1338d58a68dce5
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-17 13:14:13 +0200

    Finalize login section of the login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+      },
+      backgroundColor: {
+        "memoir-light": "rgb(245,241,237)",
+        "memoir-dark": "rgb(80,78,77)",
+      },
+      borderColor: {
+        "memoir-b-dark": "rgb(70,64,61)",
+      },
+      colors: {
+        "memoir-light": "rgb(242,223,212)",
+        "memoir-dark": "rgb(70,64,61)",
+      },
+    },
   },
   plugins: [],
 };
