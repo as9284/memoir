2d9a21033a123fc5c9a5d74a39c421a082c721c6
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-17 20:15:03 +0200

    Update env file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH,
+  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE,
   messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER,
