19ef627580592556b1fa1c3b8c04785c28e32188
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-20 19:40:46 +0200

    Adjust top margin

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -102,7 +102,7 @@ export const Notes = () => {
           placeholder="Search notes..."
           className="w-3/4 md:w-1/2 p-3 indent-2 bg-memoir-dark drop-shadow-md mb-4 text-memoir-light rounded-lg placeholder:text-neutral-400"
         />
-        <h4 className="text-lg text-memoir-dark/40 font-medium text-center select-none mt-4">
+        <h4 className="text-lg text-memoir-dark/40 font-medium text-center select-none mt-2">
           {notes.length === 0 ? "Looks empty in here..." : null}
         </h4>
 
