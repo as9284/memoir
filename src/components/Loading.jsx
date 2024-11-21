739a63f03917434bdf24c9369c7d3991d6c0ce3b
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-21 15:02:33 +0200

    Improved loading design

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -8,7 +8,7 @@ export const Loading = () => {
           size="55"
           stroke="4"
           stroke-length="0.15"
-          bg-opacity="0.1"
+          bg-opacity="0.3"
           speed="1.3"
           color="white"
         ></l-infinity>
