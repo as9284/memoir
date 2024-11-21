b05ad6bb610a802f5edb9bd1a0c5720c770180e4
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-21 15:02:11 +0200

    Improve loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Loading = () => {
   return (
-    <div className="w-full min-h-dvh flex items-center justify-center bg-memoir-light z-50">
+    <div className="fixed inset-0 w-full h-full flex items-center justify-center bg-memoir-darker z-[9999]">
       <div className="text-lg font-semibold">
         <l-infinity
           size="55"
@@ -10,7 +10,7 @@ export const Loading = () => {
           stroke-length="0.15"
           bg-opacity="0.1"
           speed="1.3"
-          color="black"
+          color="white"
         ></l-infinity>
       </div>
     </div>
